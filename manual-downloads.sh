13892be623c3dae403b7936389ef92c116519427
Ally <ally@purplish.blue>
2022-11-21 10:29:22 -0500

    remove download script from index and update it to be more robust

--- manual-downloads.sh
+++ manual-downloads.sh
@@ -1,7 +1,11 @@
 set -e
 
+
+IMPORTDIR="${HOME}/.cache/packwiz/cache/import/"
+mkdir -p "${IMPORTDIR}"
+cd "${IMPORTDIR}"
+
 # some of the mods need to be downloaded manually
-cd ~/.cache/packwiz/cache/import/
 wget https://mediafilez.forgecdn.net/files/4067/921/deeperdarker-fabric-1.1.4.jar
 wget https://mediafilez.forgecdn.net/files/3963/321/RoughlyEnoughTrades-1.19-1.0.jar
 wget https://mediafilez.forgecdn.net/files/4024/443/inmisaddon-1.0.2.jar
