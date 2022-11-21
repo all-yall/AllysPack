b006a7b2aac5144b3d9c6ea443f56b5247b7586e
Ally <ally@purplish.blue>
2022-11-21 01:49:43 -0500

    add script to get api limited mods

---
+++ manual-downloads.sh
@@ -0,0 +1,9 @@
+set -e
+
+# some of the mods need to be downloaded manually
+cd ~/.cache/packwiz/cache/import/
+wget https://mediafilez.forgecdn.net/files/4067/921/deeperdarker-fabric-1.1.4.jar
+wget https://mediafilez.forgecdn.net/files/3963/321/RoughlyEnoughTrades-1.19-1.0.jar
+wget https://mediafilez.forgecdn.net/files/4024/443/inmisaddon-1.0.2.jar
+
+
